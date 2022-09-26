d48b7a9928a37a77fcbb74ad079e6a316142ac14
hossam7amdy <hossamhamdy117@gmail.com>
2022-09-26 18:59:25 +0200

    formik-inputs

---
+++ src/components/Input/CustomInput.js
@@ -0,0 +1,25 @@
+import {
+  Input,
+  FormLabel,
+  InputGroup,
+  FormControl,
+  FormErrorMessage,
+} from '@chakra-ui/react';
+import { Field, useField } from 'formik';
+
+const CustomInput = ({ label, leftElement, ...props }) => {
+  const [field, meta] = useField(props);
+
+  return (
+    <FormControl isInvalid={meta.touched && meta.error}>
+      <FormLabel>{label || props.name}</FormLabel>
+      <InputGroup>
+        {leftElement}
+        <Input as={Field} {...props} {...field} />
+      </InputGroup>
+      <FormErrorMessage>{meta.error}</FormErrorMessage>
+    </FormControl>
+  );
+};
+
+export default CustomInput;
