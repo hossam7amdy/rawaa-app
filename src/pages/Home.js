55bffe8cb09bafa0e83bd1115f5c6caaab5a360a
hossam7amdy <hossamhamdy117@gmail.com>
2022-09-28 11:24:41 +0200

    add home-page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
-import Navbar from '../components/layout/Navbar';
+import { Box } from '@chakra-ui/react';
+
+import { Header } from '../components/layout/Header';
+import { Slider } from '../components/UI/Slider';
+import { Categories } from '../components/categories/Categories';
+import { MostOrdered } from '../components/products/MostOrdered';
+import { Footer } from '../components/layout/Footer';
+import { Navbar } from '../components/layout/Navbar';
 
 const Home = () => {
   return (
     <Box>
+      <Header />
       <Navbar />
+      <Slider />
+      <Categories />
+      <MostOrdered />
+      <Footer />
     </Box>
   );
 };
