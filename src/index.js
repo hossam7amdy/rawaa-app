14900f9a5ee54393c5ec6063cb5b1279414e75b1
hossam7amdy <hossamhamdy117@gmail.com>
2022-10-10 22:38:09 +0200

    initials

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { QueryClientProvider, QueryClient } from 'react-query';
 
 import App from './App';
 import './theme/styles.css';
+
+import store from './context/store';
 import theme from './theme/theme.js';
-import AuthProvider from './context/auth';
+import AuthProvider from './context/AuthContext';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <AuthProvider>
       <ChakraProvider theme={theme}>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <Provider store={store}>
+            <App />
+          </Provider>
+          <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+        </QueryClientProvider>
       </ChakraProvider>
     </AuthProvider>
   </BrowserRouter>
